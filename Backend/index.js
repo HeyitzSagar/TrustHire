b2ed1d475946d0366d336f7fba4d927c70a4cd24
Sagar Gupta <sagargupta.gola@gmail.com>
2024-04-08 23:20:20 +0530

    fixes errors and added cloud-api cloudinary

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,27 @@
-import express from 'express';
-import connectDB from './database/db.js';
+import express from "express";
+import connectDB from "./database/db.js";
+import { configDotenv } from "dotenv";
+import {v2 as cloudinary} from 'cloudinary';
+import cors from 'cors';
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME, 
+  api_key: process.env.CLOUDINARY_CLIENT_API, 
+  api_secret: process.env.CLOUDINARY_CLIENT_API_SECRET, 
+});
+configDotenv();
+connectDB();
 const app = express();
-
-connectDB(app);
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.use(cors({
+  origin:[process.env.FRONTEND_URL],
+  methods:['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+  credentials: true
+}))
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
